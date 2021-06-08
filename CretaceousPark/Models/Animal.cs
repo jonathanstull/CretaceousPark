e080e706a6fb04162603af085bdae208c4c6052c
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-08 07:31:16 -0700

    adds data annotations

--- CretaceousPark/Models/Animal.cs
+++ CretaceousPark/Models/Animal.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CretaceousPark.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    
+    [Required]
+    [StringLength(25)]
     public string Name { get; set; }
+    
+    [Required]
     public string Species { get; set; }
+    
+    [Required]
+    [Range(0, 100, ErrorMessage = "Age must be between 0 and 100")]
     public int Age { get; set; }
+
+    [Required]
     public string Gender { get; set; }
   }
 }
